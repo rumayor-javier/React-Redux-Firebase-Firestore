56273c752d4db8dba391faca56daff2c78be25cf
Javier <rumayor.javier@gmail.com>
2023-04-12 19:41:36 -0600

    test 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { JournalApp } from "./journalApp";
+import { JournalApp } from "./JournalApp.jsx";
 import "./styles.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
